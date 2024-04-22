dfb9325e6076ebae3f9703f2b12297f7a598297c
Oscar Rubio <ofrubio0@gmail.com>
2024-04-22 15:51:49 -0500

    (UPDATE) Se crea funcion downloadBase64ImageAsPng para descargar imagen en base64 para la Edicion

--- src/helpers/download-image-as-png.ts
+++ src/helpers/download-image-as-png.ts
@@ -25,8 +25,29 @@ export const downloadImageAsPng = async(url:string)=>{
         .ensureAlpha()
         .toFile(completePath)
 
-    return completePath
+    return imageNamePng
         
 
 
+}
+
+export const downloadBase64ImageAsPng = async(base64Image: string)=>{
+    //remover encabezado
+    base64Image = base64Image.split(';base64,').pop()
+    const imageBuffer = Buffer.from(base64Image, 'base64')
+
+    const folderPath = path.resolve('./','./generated/images/')
+    fs.mkdirSync(folderPath, {recursive: true})
+
+    const imageNamePng = `${new Date().getTime()}-64.png`
+
+    // Transformar a RGBA, png // Asi lo espera OpenAI
+    const completePath = path.join(folderPath, imageNamePng)
+    
+    await sharp(imageBuffer)
+        .png()
+        .ensureAlpha()
+        .toFile(completePath)
+
+    return imageNamePng
 }
