2f435ee90548d0fa8d5c7b3f4c86c9ac57e54583
Oscar Rubio <ofrubio0@gmail.com>
2024-04-21 20:39:37 -0500

    (ADD) se crea el imageGenerationUseCase para generacion de imagenes

---
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -0,0 +1,33 @@
+import OpenAI from "openai";
+import { downloadImageAsPng } from '../../helpers/download-image-as-png';
+
+interface Options{
+    prompt: string
+    originalImage?: string
+    maskImage?:string
+}
+
+
+export const imageGenerationUseCase = async(openAi: OpenAI, options: Options)=>{
+
+    const {prompt, originalImage, maskImage} = options
+
+    const response = await openAi.images.generate({
+        prompt: prompt,
+        model: 'dall-e-3',
+        n: 1,
+        size: '1024x1024',
+        quality: 'standard',
+        response_format: 'url'
+    })
+
+    await downloadImageAsPng(response.data[0].url)
+    
+
+    return{
+        url: response.data[0].url,
+        localPath: '',
+        revised_prompt: response.data[0].revised_prompt
+    }
+
+}
